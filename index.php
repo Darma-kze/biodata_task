e4394cd90cc5b577c33405ce51da37d494e9dc74
Darma <darma.darma2506@gmail.com>
2024-09-04 19:11:11 +0700

    second commit updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Biodata</title>
+    <title>Form Biodata Pweb</title>
     <link rel="stylesheet" href="style.css">
     <script>function showSuccessPopup() {
             alert('Data has been submitted successfully!');
@@ -18,22 +18,22 @@
         }
         ?>
         <form action="submit.php" method="POST">
-    <label for="nama">Nama:</label>
+    <label for="nama">Nama: </label>
     <input type="text" id="nama" name="nama" required><br>
 
-    <label for="nim">NIM:</label>
+    <label for="nim">NIM: </label>
     <input type="text" id="nim" name="nim" required><br>
 
-    <label for="kelas">Kelas:</label>
+    <label for="kelas">Kelas: </label>
     <input type="text" id="kelas" name="kelas" required><br>
 
-    <label for="gender">Gender:</label>
+    <label for="gender">Gender: </label>
     <input type="text" id="kelas" name="kelas" required><br>
 
-    <label for="email">Email:</label>
+    <label for="email">Email: </label>
     <input type="email" id="email" name="email" required><br>
 
-    <label for="pesan">Pesan:</label>
+    <label for="pesan">Pesan: </label>
     <textarea id="pesan" name="pesan"></textarea><br>
 
     <input type="submit" value="Kirim">
